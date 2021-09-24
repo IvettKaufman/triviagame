04b260ae689d35c2af08edc0868d2d5d505b210b
Amirfy <83363564+Amirfy@users.noreply.github.com>
2021-09-24 11:19:38 +0100

    update

---
+++ functions/controllers/playerChoice.js
@@ -0,0 +1,23 @@
+const functions = require("firebase-functions");
+const express = require("express");
+const admin = require("firebase-admin");
+
+const playerChoiceApp = express();
+
+// receives: a.gameId b.playerId c.currentQuestion
+playerChoiceApp.post("/setPlayerChoice", (req, res) => {
+    //  1. write player move
+    admin.firestore().collection("games").doc(req.body.gameId).collection("players").doc(req.body.playerId).update({
+        choice: req.body.choice,
+        currentQuestion: req.body.currentQuestion,
+        choiceTime: Date.now()
+    }).then(() => {
+        res.status(200).send("Player choice successfully set.")
+    }).catch((error) => {
+        console.error("Error updating document: ", error);
+        res.status(400).send('Something broke!');
+    });
+});
+
+
+exports.api = functions.https.onRequest(playerChoiceApp);
--- functions/index.js
+++ functions/index.js
@@ -2,5 +2,6 @@ module.exports = {
   ...require("./controllers/joining.js"),
   ...require("./controllers/numberQuestion.js"),
   ...require("./controllers/landActions.js"),
+  ...require("./controllers/playerChoice.js"),
   ...require("./controllers/triviaQuestion.js")
-};
+}
